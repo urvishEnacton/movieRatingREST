c5267393cb9e45ab5205e507a5e71e0001fded0f
urvishEnacton <126880242+urvishEnacton@users.noreply.github.com>
2023-04-10 10:50:30 +0530

    Add files via upload

--- src/auth.js/index.js
+++ src/auth.js/index.js
@@ -33,13 +33,4 @@ export const adminAuth = async (req, res, next) => {
   }
 };
 
-// const token = req.headers["x-token"];
-// if (token) {
-//   try {
-//     const me = await jwt.verify(token, process.env.SECRET);
-//     return await models.User.findById(me.id).populate("roleId");
-//   } catch (e) {
-//     throw new AuthenticationError("Session Invalid or expired.");
-//   }
-// }
-// };
+
